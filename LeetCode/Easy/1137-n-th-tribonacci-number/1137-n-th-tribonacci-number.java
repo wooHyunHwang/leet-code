2bbea2335efd385e141694a52427cfecda8cedca
wooHyunHwang <45251387+wooHyunHwang@users.noreply.github.com>
2023-01-30 23:51:10 +0900

    Time: 0 ms (100.00%), Space: 39.2 MB (52.50%) - LeetHub

--- LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ LeetCode/Easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,6 +1,26 @@
 class Solution {
     public int tribonacci(int n) {
-        
+
+        // int 교체 방식, 공간 복잡도 효율 ↑
+
+        int 일 = 0;
+        int 이 = 1;
+        int 삼 = 1;
+
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        for (int i = 3; i <= n; ++i) {
+            int 임시 = 일 + 이 + 삼;
+
+            일 = 이;
+            이 = 삼;
+            삼 = 임시;
+        }
+
+        return 삼;
+
+        /* 배열 방식
         int[] tri;
 
         if (n <= 2) {
@@ -21,6 +41,7 @@ class Solution {
         }
 
         return tri[n];
+        */
 
         // n = (n+3) - (n+1) - (n+2)
     }
