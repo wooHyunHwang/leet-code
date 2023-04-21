723f5391e98187bfb83f1060bb7238ee3601b835
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2023-04-21 23:04:04 +0900

    Time: 153 ms (25.00%), Space: 33.3 MB (100.00%) - LeetHub

---
+++ LeetCode/Easy/0504-base-7/0504-base-7.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun convertToBase7(num: Int): String {
+        if(num == 0) return "0";
+
+        val sb: StringBuilder = StringBuilder()
+        val minus: Boolean = num < 0
+        var absNum = Math.abs(num)
+
+        while(absNum > 0) {
+            sb.append(absNum % 7)
+            absNum /= 7 
+        }
+
+        if(minus) {
+            sb.append("-")
+        }
+
+        return sb.reverse().toString()
+    }
+}
