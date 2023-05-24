044e8d17f7c4191a85e47ac8fcf6db3b92ff0394
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2023-05-24 23:04:03 +0900

    Time: 26 ms (71.21%), Space: 50.5 MB (80.83%) - LeetHub

---
+++ LeetCode/Easy/0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,25 @@
+class RecentCounter {
+
+    private LinkedList<Integer> counter;
+
+    public RecentCounter() {
+        this.counter = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+
+        this.counter.addLast(t + 3000);
+        
+        while(this.counter.getFirst() < t) {
+            this.counter.remove();
+        }
+
+        return this.counter.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
