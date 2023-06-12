4aad3c736be8dd019f3c16cff7433bfd08f52d6a
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2023-06-12 23:35:07 +0900

    Time: 184 ms (38.46%), Space: 37.4 MB (100.00%) - LeetHub

---
+++ LeetCode/Easy/0598-range-addition-ii/0598-range-addition-ii.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun maxCount(m: Int, n: Int, ops: Array<IntArray>): Int {
+        
+        if(ops.size == 0) return m * n
+        
+        var leftMin = m
+        var rightMin = n
+        
+        for(i in 0 .. ops.size - 1) {
+            leftMin = Math.min(leftMin, ops[i][0])
+            rightMin = Math.min(rightMin, ops[i][1])
+        }
+        
+        return leftMin * rightMin
+    }
+}
