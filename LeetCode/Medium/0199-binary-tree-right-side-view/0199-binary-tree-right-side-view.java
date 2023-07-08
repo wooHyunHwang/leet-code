cb036881e15babf00aaa90ac3fa5ca9b2693aa8c
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2023-07-09 00:08:04 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (73.89%) - LeetHub

--- LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,14 +14,12 @@
  * }
  */
 class Solution {
-    Map<Integer, List<TreeNode>> levelMap = null;
     public List<Integer> rightSideView(TreeNode root) {
 
-        levelMap = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
 
-        this.dfs(root, ans, 1);
+        this.dfs(root, ans, 0);
         
         return ans;
     }
@@ -30,14 +28,7 @@ class Solution {
 
         if(node == null) return;
 
-        List<TreeNode> curReverse = levelMap.getOrDefault(level, new ArrayList<>());
-        curReverse.add(node);
-        levelMap.put(
-            level,
-            curReverse
-        );
-
-        if(curReverse.size() == 1) ans.add(curReverse.get(0).val);
+        if(ans.size() == level) ans.add(node.val);
 
         this.dfs(node.right, ans, level + 1);
         this.dfs(node.left, ans, level + 1);
