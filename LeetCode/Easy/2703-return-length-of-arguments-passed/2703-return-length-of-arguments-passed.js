6bdb554ed9cc7969c7543bcb79f56ded0318616d
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2023-11-26 22:13:47 +0900

    Time: 47 ms (78.96%), Space: 41.8 MB (64.85%) - LeetHub

---
+++ LeetCode/Easy/2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    var cnt = 0;
+	for(const a in args) {
+        cnt++;
+    }
+    return cnt;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
