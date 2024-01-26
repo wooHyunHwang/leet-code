aaddf1a7466718825f7a0efb7b6f2f4312949b2e
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-01-26 21:59:38 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (5.95%) - LeetHub

--- LeetCode/Easy/1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
+++ LeetCode/Easy/1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
@@ -1,6 +1,16 @@
 class Solution {
     public int sumBase(int n, int k) {
         int ans = 0;
+
+        while(n != 0) {
+            ans += n % k;
+            n /= k;
+        }
+
+        return ans;
+    }
+    public int sumBase2(int n, int k) {
+        int ans = 0;
         int div = (int)Math.pow(k, 7);
 
         while(n != 0) {
