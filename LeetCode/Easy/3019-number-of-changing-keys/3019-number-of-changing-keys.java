08d5b0f75542d95135cba5e772bdaa2675991a22
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-01-29 21:06:42 +0900

    Time: 1 ms (100.00%), Space: 42.3 MB (100.00%) - LeetHub

---
+++ LeetCode/Easy/3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -0,0 +1,18 @@
+class Solution {
+    private final int div = 32;
+    public int countKeyChanges(String s) {
+
+        int ans = 0;
+        char[] cAry = s.toCharArray();
+        int beforeUpperCase = cAry[0] >= 'a' ? cAry[0] - div : cAry[0];
+
+        for (int i = 1; i < cAry.length; ++i) {
+            int nextUpperCase = cAry[i] >= 'a' ? cAry[i] - div : cAry[i];
+            if (beforeUpperCase != nextUpperCase) ans++;
+            // System.out.println(beforeUpperCase + " + " + nextUpperCase);
+            beforeUpperCase = nextUpperCase;
+        }
+
+        return ans;
+    }
+}
