64617f629a72cce475c19c9e17082b1350a8c3b8
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-02-12 23:11:02 +0900

    Time: 2 ms (100.00%), Space: 45.4 MB (50.00%) - LeetHub

--- LeetCode/Easy/3033-modify-the-matrix/3033-modify-the-matrix.java
+++ LeetCode/Easy/3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -3,6 +3,35 @@ class Solution {
 
         int[][] answer = new int[matrix.length][matrix[0].length];
         int[] maximum = new int[matrix[0].length];
+        List<Integer> stack = new ArrayList<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[i][j] != -1) {
+                    answer[i][j] = matrix[i][j];
+                }
+                else {
+                    stack.add(i);
+                    stack.add(j);
+                }
+
+                if (maximum[j] < matrix[i][j]) maximum[j] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < stack.size(); i += 2) {
+
+            answer[stack.get(i)][stack.get(i+1)] = maximum[stack.get(i+1)];
+        }
+
+
+        return answer;
+    }
+    public int[][] modifiedMatrix2(int[][] matrix) {
+
+        int[][] answer = new int[matrix.length][matrix[0].length];
+        int[] maximum = new int[matrix[0].length];
         Queue<Integer> stack = new LinkedList<>();
 
         for (int i = 0; i < matrix.length; i++) {
