349382f7a7c89c8f90ca948d8ad58ca901f1b797
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-02-19 22:41:01 +0900

    Time: 1 ms (52.33%), Space: 41.7 MB (38.15%) - LeetHub

---
+++ LeetCode/Easy/2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        StringBuilder sb = new StringBuilder();
+        boolean reverse = false;
+        for (char c : word.toCharArray()) {
+
+            sb.append(c);
+
+            if (!reverse && c == ch) {
+                reverse = true;
+                sb.reverse();
+            }
+        }
+
+        return sb.toString();
+    }
+}
