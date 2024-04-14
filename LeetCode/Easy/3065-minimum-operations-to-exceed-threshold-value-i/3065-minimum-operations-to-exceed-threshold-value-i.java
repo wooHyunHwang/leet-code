8c92e10fea723b72ae581ef791321d0ac57528d4
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-04-14 22:29:05 +0900

    Time: 0 ms (100.00%), Space: 42.7 MB (47.89%) - LeetHub

---
+++ LeetCode/Easy/3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int count = 0;
+
+        for (int n : nums) {
+            if (n < k) count++;
+        }
+        return count;
+    }
+}
