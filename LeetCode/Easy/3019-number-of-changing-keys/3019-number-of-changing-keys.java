f63adfc88a76f945265f9418ab61c2b5ce6867c9
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-05-23 23:12:12 +0900

    Time: 1 ms (99.93%), Space: 41.7 MB (89.28%) - LeetHub

--- LeetCode/Easy/3019-number-of-changing-keys/3019-number-of-changing-keys.java
+++ LeetCode/Easy/3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -11,7 +11,7 @@ class Solution {
             if (beforeUpperCase != nextUpperCase) ans++;
             // System.out.println(beforeUpperCase + " + " + nextUpperCase);
             beforeUpperCase = nextUpperCase; 
-        }
+        }  
 
         return ans;
     }
