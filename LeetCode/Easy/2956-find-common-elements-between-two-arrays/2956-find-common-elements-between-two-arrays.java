136eefea06f7dbf4ea76bf90819ac2303b8c65f6
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-08-30 22:41:26 +0900

    Time: 1 ms (100.00%), Space: 45.7 MB (17.51%) - LeetHub

---
+++ LeetCode/Easy/2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+        int[] ans = new int[2];
+        int[] num1Count = new int[101];
+        boolean[] num1Checked = new boolean[101];
+
+        for (int n : nums1) num1Count[n]++;
+
+        for (int n2 : nums2) { 
+            if (num1Count[n2] > 0) {
+                ans[1]++;
+                if (!num1Checked[n2]) {
+                    num1Checked[n2] = true;
+                    ans[0] += num1Count[n2];
+                }
+            }
+        }
+
+        return ans;
+    }
+}
