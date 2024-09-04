7748533178079800bfcf99d641e0ff82aef37884
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-09-05 00:01:00 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (87.53%) - LeetHub

--- LeetCode/Easy/2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ LeetCode/Easy/2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -1,6 +1,19 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
         int count = 0;
+        int diff2 = diff * 2;
+        boolean[] history = new boolean[251];
+
+        for (int n : nums) history[n] = true;
+
+        for (int n : nums) {
+            if (history[n+diff] && history[n+diff2]) count++;
+        }
+
+        return count;
+    }
+    public int arithmeticTriplets2(int[] nums, int diff) {
+        int count = 0;
         
         for (int i = 0; i < nums.length; ++i) {
             for (int j = i+1; j < nums.length; ++j) {
