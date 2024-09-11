cd75be383ccc03a400e487e257adecb7f9edaccc
WooHyeon Hwang <45251387+wooHyunHwang@users.noreply.github.com>
2024-09-11 22:31:55 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (90.99%) - LeetHub

---
+++ LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int count = 0;
+
+        while(start > 0 || goal > 0) {
+
+            if (start % 2 != goal % 2) {
+                count++;
+            }
+
+            start /= 2;
+            goal /= 2;
+        }
+
+        return count;
+    }
+}
